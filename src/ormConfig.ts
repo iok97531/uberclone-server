ea59418ce93d9af33a2f7a3d20463e3aaef37020
ugl5 <iok97531@gmail.com>
2020-02-24 19:15:28 +0900

    DB_NAME env variable

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -1,17 +1,17 @@
 import dotenv from 'dotenv';
 import { ConnectionOptions } from 'typeorm';
 dotenv.config();
- 
+
 const connectionOptions: ConnectionOptions = {
-    type: "postgres",
-    database: "uberclone",
-    synchronize: true,
-    logging: true,
-    entities: ["entities/**/*.*"],
-    host: process.env.DB_ENDPOINT,
-    port: 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD
+  type: "postgres",
+  database: process.env.DB_NAME || "uberclone",
+  synchronize: true,
+  logging: true,
+  entities: ["entities/**/*.*"],
+  host: process.env.DB_ENDPOINT,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
 };
 
 export default connectionOptions;
